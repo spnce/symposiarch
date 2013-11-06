88c78cba01a66670c87c004178e9cb7a8ca4b641
spnce <snmcnamara@gmail.com>
2013-11-06 14:44:59 -0800

    Delete subject.py

--- subject.py
+++
@@ -1,7 +0,0 @@
-class Subject:
-
-    def __init__(self, weight, gender, hunger, tolerance):
-        self.weight = weight
-        self.gender = gender
-        self.hunger = hunger
-        self.tolerance = tolerance
