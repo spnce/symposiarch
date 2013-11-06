831d052596380047ca6230e9f39d406ab17b26d7
Spencer McNamara <smcnamara@smcnamara-m01.ad.savagebeast.com>
2013-11-06 11:50:14 -0800

    initial

---
+++ drink_action.py
@@ -0,0 +1,7 @@
+class DrinkAction:
+
+    def __init__(self, subject, measurements):
+        self.subject = subject
+        self.measurements = measurements
+
+    def get(self):
---
+++ subject.py
@@ -0,0 +1,7 @@
+class Subject:
+
+    def __init__(self, weight, gender, hunger, tolerance):
+        self.weight = weight
+        self.gender = gender
+        self.hunger = hunger
+        self.tolerance = tolerance
