8901057c9a2080ce3f34490ad359e49104230a24
Sam Douglass <sdouglassdev@gmail.com>
2013-11-06 15:27:17 -0800

    mock implementation of the arduino reader, just sleeps then returns a random number

--- symposiarch/lib/arduino_reader.py
+++ symposiarch/lib/arduino_reader.py
@@ -1,3 +1,5 @@
+import random
+import time
 import serial
 
 
@@ -5,3 +7,11 @@ class ArduinoReader:
 
     def __init__(self):
         pass
+
+    def estimate_blood_alcohol_concentration(self, seconds):
+        # read data from the arduino alcohol sensor for a number of seconds
+        time.sleep(seconds)
+        # process the data into a blood alcohol concentration estimate
+        bac = random.randint(0, 1000) / 1000
+        # return the BAC estimate
+        return bac
