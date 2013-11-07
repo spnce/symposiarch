de01ef193f96d28d90e6cc4e167afab651163181
anybakke <amelia2009@gmail.com>
2013-11-06 17:27:31 -0800

    merge

--- lib/drink_action.py
+++ lib/drink_action.py
@@ -5,14 +5,9 @@ class DrinkAction:
         self.measurements = measurements
 
     def get(self):
-<<<<<<< HEAD
     	hours = 1
     	gender_constant = 0.58 if self.drinker.gender else 0.49
     	adjusted_weight = self.drinker.weight * 0.453592 * gender_constant
     	metabolized = (0.01 + 0.005 * self.drinker.tolerance) * hours
     	drinks = (0.138 - measurements['current_bac']) * adjusted_weight / (0.806 * 1.2) - metabolized
     	return drinks
-    	
-=======
-        pass
->>>>>>> 9ae9b410a393c9a2e81e4441ca91cd01c6123ce9
